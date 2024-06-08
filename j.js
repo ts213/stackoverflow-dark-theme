b8e63a03bd6957e11d9b03076992af64e4794477
ts213 <107356620+ts213@users.noreply.github.com>
2024-06-08 15:23:47 +0300

    add top bar hiding

--- j.js
+++ j.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   if (document.body.classList.contains('question-page')) {
-    hideBars();
+    browser.storage.local.get(['isHideBars', 'isRemoveHeader']).then(obj => {
+      if (obj.isHideBars) hideBars();
+      if (obj.isRemoveHeader) removeHeader();
+    });
   }
 });
-function hideBars() {
-  browser.storage.local.get('isHideBars').then(obj => {
-    if (!obj.isHideBars) return;
 
-    document.getElementById('mainbar').style.width = '70vw';
-    document.getElementById('left-sidebar').style.display = 'none';
-    document.getElementById('sidebar').style.display = 'none';
-  });
+function hideBars() {
+  document.getElementById('mainbar').style.width = '70vw';
+  document.getElementById('left-sidebar').style.display = 'none';
+  document.getElementById('sidebar').style.display = 'none';
+}
+
+function removeHeader() {
+  document.getElementsByTagName('header')[0].remove();
+  document.body.style.paddingTop = '0';
 }
--- opts.js
+++ opts.js
@@ -1,15 +1,23 @@
-browser.storage.local.get('isHideBars').then(obj => {
+browser.storage.local.get(['isHideBars', 'isRemoveHeader']).then(obj => {
   const isHideBarsBox = document.getElementById('isHideBars');
-  isHideBarsBox.addEventListener('click', setHideBars);
+  const isRemoveHeaderBox = document.getElementById('isRemoveHeader');
   isHideBarsBox.checked = Boolean(obj.isHideBars);
+  isRemoveHeaderBox.checked = Boolean(obj.isRemoveHeader);
+
+  const optsWrap = document.getElementById('optsWrap');
+  optsWrap.addEventListener('click', (ev) => {
+    if (['isHideBars', 'isRemoveHeader'].includes(ev.target.id)) {
+      togglePersistentOption(ev.target.checked, ev.target.id);
+    }
+  });
 });
 
-async function setHideBars(ev) {
-  if (ev.target.checked) {
-    browser.storage.local.set({ 'isHideBars': true });
+async function togglePersistentOption(isChecked, id) {
+  if (isChecked) {
+    browser.storage.local.set({ [id]: true });
     toggleReminder();
   } else {
-    browser.storage.local.remove('isHideBars');
+    browser.storage.local.remove(id);
     toggleReminder(true);
   }
 }
